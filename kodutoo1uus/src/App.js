ce9ca92d28f32fa9941ef30264cbb86d99034185
Kristi J <johanson090@hotmail.com>
2023-02-22 23:07:02 +0200

    Kodutoo 1osa

---
+++ kodutoo1uus/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <button className="Nupuke">Nupuke</button>
+      <p className="Oluline jutt">
+        <em> Oluline jutt</em>
+      </p>
+      <img className="pilt" src="https://lscdn.azureedge.net/products/computer/images/Dell-3048-19.5-Inch-Desktop-PC.jpg" alt="arvuti" />
+    </div>
+  );
+}
+
+export default App;
--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,30 +1,14 @@
 import './App.css';
 
 import { Link, Route, Routes } from 'react-router-dom';
-// tag --> <div>   </div>   komplektide jaoks
-// <img /> self-closing    tema omadused lähevadki tema sisse
-// button ---> on algus ja lõpp, tema sisse kirjutatakse midagi
 
 function App() {
   return (
     <div className="App">
-      <Link to="/avaleht">
-        <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/7/74/A-Cat.jpg" alt="" />
-      </Link>
+      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/7/74/A-Cat.jpg" alt="" />
+      <button className="nupp">nupp</button>
 
-      <Link to="/ostukorv">
-        <button className="nupp">nupp</button>
-      </Link>
-
-      <Link to="/lisa-toode">
-        <button className="nupp">Toodet lisama</button>
-      </Link>
-
-      <Routes>
-          <Route path="avaleht" element={<div>123</div>}/>
-          <Route path="ostukorv"element={<div>0</div>}/>
-          <Route path="lisa-toode"element={<div>LT</div>}/>
-      </Routes>
+      <Routes></Routes>
     </div>
   );
 }
