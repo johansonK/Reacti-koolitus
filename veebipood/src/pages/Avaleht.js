c517df382366a13306cb70108abb296fed94eb5f
Kristi J <johanson090@hotmail.com>
2023-02-23 11:24:18 +0200

    pages+failide import, useState dunaamiline valjkuvamine &&

---
+++ veebipood/src/pages/Avaleht.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+function Avaleht() {
+  const[kogus, uuendaKogus] = useState(8); //number (number) - kui tehakse arvutusi: kogus, hinnad, summa
+  const[sonum, uuendaSonum] = useState("Uuenda kogust!"); //sonaline (string) - koik ulejaanud: telefoninumbrid, isikukood, sonad, nimed
+  const [laigitud, uuendaLaigitud] = useState(false); //kahendvaartus (boolean): sisselogitud, makstud, taisealine (eelnevad kolm on koigis keeltes samad)
+
+  
+  function nulli() {
+    uuendaKogus(0);
+    uuendaSonum("Panid tagasi nulli");
+  }
+
+  function vahenda() {
+    uuendaKogus(kogus - 1);
+    uuendaSonum("Vahendasid kogust");
+  }
+
+  function suurenda() {
+    uuendaKogus(kogus + 1);
+    uuendaSonum("Suurendasid kogust");
+  }
+
+
+  return (
+    <div>
+      {laigitud === true && <img src="/laigitud.svg" alt="" />}
+      {laigitud === false && <img src="/mittelaigitud.svg" alt="" />}
+      <button onClick={() => uuendaLaigitud(!laigitud)}>Muuda laigitut</button>
+      <div>{sonum}</div>
+      { kogus > 0 && <button onClick={nulli}>Nulli</button>}
+      <button disabled={kogus === 0} onClick={vahenda}>-</button>
+      {kogus}
+      <button onClick={suurenda}>+</button>
+    </div>
+  )
+}
+
+export default Avaleht
---
+++ veebipood/src/pages/Ostukorv.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Ostukorv() {
+  return (
+    <div>
+         Ostukorv on tuhi.
+        <Link to="/">Tooteid lisama</Link>
+
+
+    </div>
+  )
+}
+
+export default Ostukorv
