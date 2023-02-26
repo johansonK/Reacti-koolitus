56e75874b2e73bfcff46f92cab899344ea0c3c74
Kristi J <johanson090@hotmail.com>
2023-02-26 12:08:48 +0200

    kontakt ja meist muudatused

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,10 +1,31 @@
-
+import { useState } from 'react';
 function Kontakt() {
+
+const [naitaTelPuiestee, muudaNaitaTelPuiestee] = useState(false);
+const [naitaTelKase, muudaNaitaTelKase] = useState(false);
+const [naitaTelSakala, muudaNaitaTelSakala] = useState(false);
+
+
   return (
   <div>
     <div>See on kontaktide leht, nahtav localhost:3001/kontakt aadressil</div>
-    <div>Asume aadressil Puiestee 5</div>
+    <br />
+    <div>Asume aadressidel: </div>
+    <br />
+    <div onClick={() => muudaNaitaTelPuiestee(!naitaTelPuiestee)}>Puiestee 5</div>
+    <div>Rapla</div>
+    {naitaTelPuiestee && <div>5782939</div>}
+    <br />
+    <div onClick={() => muudaNaitaTelKase(!naitaTelKase)}>Kase 7</div>
+    <div>Keila</div>
+    {naitaTelKase && <div>5889990</div>}
+    <br />
+    <div onClick={() => muudaNaitaTelSakala(!naitaTelSakala)}>Sakala 5</div>
+    <div>Tori</div>
+    {naitaTelSakala && <div>58899111</div>}
+    <br />
     <div>Kiirete probleemide puhul helistage 53535353</div>
+    <br />
     <div>Tartus asume Tamme 38</div>
   </div>
   )
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,9 +1,30 @@
+import {useState} from 'react';
 
 function Meist() {
+
+const [ kontakt, naitaKontakt] = useState("");
+
   return (
   <div>
     <div>See on meist leht, nahtav localhost:3001/meist aadressil</div>
     <div>Oleme Baltiriikide tuntuim uudiste ettevõte</div>
+    <br />
+    <div>Meie tootajad:</div>
+    <br />
+    <div>Tiina Teha</div>
+    <div>Reporter</div>
+    <button onClick={() => naitaKontakt('55577788')}>Vota uhendust</button>
+    <br /> <br />
+    <div>Anne Anna</div>
+    <div>Ilmateadustaja</div>
+    <button onClick={() => naitaKontakt('57382940')}>Vota uhendust</button>
+    <br /> <br />
+    <div>Toomas Puu</div>
+    <div>Produtsent</div>
+    <button onClick={() => naitaKontakt('54321654')}>Vota uhendust</button>
+    <br /> <br />
+     { kontakt !=="" && <div>Tema kontakt: {kontakt}</div>}
+
   </div>
   )
 }
