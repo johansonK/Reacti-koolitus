a20b4b5f81a286f9670b2f28457d5431f0efb9e3
Kristi J <johanson090@hotmail.com>
2023-02-27 12:01:48 +0200

    useRef, localStorage, useState,

--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useRef, useState } from "react";
 
-function LisaToode() {
+function LisaToode() { //Reacti hookid
+const [sonum, uuendaSonum] = useState("Lisa uus toode!");
+const inputiLuger = useRef(); // reference ehk viide inputist
+
+//function lisa2() {}
+
+const lisa = () => {
+  if (inputiLuger.current.value === "") {
+    uuendaSonum("Tuhja nimetusega ei saa toodet lisada!");
+  } else {
+    uuendaSonum("Toode lisatud " + inputiLuger.current.value);
+  }
+  
+}
   return (
-    <div>LisaToode</div>
+    <div>
+      <div>{sonum}</div> 
+      <label>Uue toote nimi</label> <br />
+      <input ref={inputiLuger} type="text" /> <br />
+      <button onClick={lisa}>Sisesta</button> <br />
+    </div>
   )
 }
 
+//<button onClick={() => lisa()}>Sisesta<button>
+//voi
+//<button onClick={lisa}>Sisesta<button>
+//MITTE:
+////<button onClick={lisa()}>Sisesta<button>
+
 export default LisaToode
+
+
