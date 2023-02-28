b2ad945bf80fd491602c1943b35965d84090e348
Kristi J <johanson090@hotmail.com>
2023-02-28 10:54:16 +0200

    kordamine localStorage ja save

--- kodutoo1uus/src/pages/Seaded.js
+++ kodutoo1uus/src/pages/Seaded.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {useState} from 'react'
 
 function Seaded() {
+  const [kujundus, muudaKujundus] = useState(localStorage.getItem("veebilehe_kujundus"));
+
+  const tumeLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "dark_mode");
+    muudaKujundus("dark_mode")
+  }
+  const heleLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "light_mode");
+    muudaKujundus("light_mode")
+  }
+  const varvilineLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "colored_mode")
+    muudaKujundus("colored_mode")
+  }
+
   return (
-    <div>Seaded</div>
+    <div>
+      <button onClick={tumeLeht}>Tume leht</button>
+      <button onClick={heleLeht}>Hele leht</button>
+      <button onClick={varvilineLeht}>Varviline leht</button>
+      { kujundus === "dark_mode" && <div>TUME LEHT</div> }
+      { kujundus === "light_mode" && <div>HELE LEHT</div> }
+      { kujundus === "colored_mode" && <div>VARVILINE LEHT</div>}
+    </div>
   )
 }
 
