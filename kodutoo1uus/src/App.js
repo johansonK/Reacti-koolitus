c706ac937124344ebe46612ceaa57e884215a4a0
Kristi J <johanson090@hotmail.com>
2023-02-28 09:59:28 +0200

    useRef, useState,

--- kodutoo1uus/src/App.js
+++ kodutoo1uus/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useRef } from 'react'
 import { Routes, Link, Route } from 'react-router-dom';
 import Avaleht from './pages/Avaleht';
 import Meist from './pages/Meist';
@@ -6,12 +7,49 @@ import Kontakt from './pages/Kontakt';
 import Seaded from './pages/Seaded';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123") {
+      muudaSisselogitud("jah");
+      muudaSonum(kasutajaNimiRef.current.value + " ,oled sisselogitud");
+    } else {
+      muudaSonum("Vale parool")
+    }
+  }
+  const logiValja = () => {
+    muudaSisselogitud("ei");
+    muudaSonum("");
+  }
+
   return (
     <div className="App">
+      <div>{sonum}</div>
+      {sisselogitud === "ei" && <div>
+        <label>Kasutajanimi</label> <br />
+        <input ref={kasutajaNimiRef} type="text" /><br />
+        <label>Parool</label> <br />
+        <input ref={paroolRef} type="password" /><br />
+      </div> }
+
+      { sisselogitud === "ei" &&  <button onClick={logiSisse}>Logi sisse</button>} <br />
+      { sisselogitud === "jah" && <button onClick={logiValja}>Logi valja</button>} <br />
+
       
+
+      
+
+     
+      
+
+
+
       <p className="Oluline jutt">
-        <em> Oluline jutt</em>
-      </p>
+        <em> Oluline jutt</em> 
+      </p> <br /><br />
       <img className="pilt" src="https://lscdn.azureedge.net/products/computer/images/Dell-3048-19.5-Inch-Desktop-PC.jpg" alt="arvuti" />
 
       <Link to="/">
