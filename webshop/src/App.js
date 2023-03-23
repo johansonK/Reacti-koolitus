236d7991e1ac056e2ef99145853ab2c7f274dd51
Kristi J <johanson090@hotmail.com>
2023-03-23 22:03:42 +0200

    webshop uus

--- webshop/src/App.js
+++ webshop/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import HomePage from "./pages/HomePage";
+import Cart from "./pages/Cart";
+import Shops from "./pages/Shops";
+import ContactUs from "./pages/ContactUs";
+import AdminHome from "./pages/AdminHome";
+import AddProduct from "./pages/AddProduct";
+import MaintainProducts from "./pages/MaintainProducts";
+import MaintainShops from "./pages/MaintainShops";
+import MaintainCategories from "./pages/MaintainCategories";
 
 function App() {
 
-  //npm install react-router-dom
-  //BrowserRouter  index.js faili
-  //siia faili <Routes>
-  //tehke failid, mida naete element sees "pages"
-  //igayhe sisse rfce
-  //Link veel ei tee
-  // kustutage yleliigsed failid
+
   //Firebase yles
-  //lisage react-toastify
+ 
 
 
   return (
     <div className="App">
       <Routes>
-        <Route path="" element={ <HomePage /> } />
+        <Route path="" element={ < HomePage/> } />
         <Route path="cart" element={ <Cart /> } />
         <Route path="shops" element={ <Shops /> } />
         <Route path="contact" element={ <ContactUs /> } />
@@ -25,7 +30,7 @@ function App() {
         <Route path="admin/add-product" element={ <AddProduct /> } />
         <Route path="admin/edit-product" element={ <HomePage /> } />
         <Route path="admin/maintain-products" element={ <MaintainProducts /> } />
-<Route path="admin/maintain-shops" element={ <MaintainShops /> } />
+        <Route path="admin/maintain-shops" element={ <MaintainShops /> } />
         <Route path="admin/maintain-categories" element={ <MaintainCategories /> } />
       </Routes>
     </div>
