0c0e7caa16cd8edc758485a87e3037273c852fe5
Kristi J <johanson090@hotmail.com>
2023-03-28 11:35:01 +0300

    tegelased useState, .map,

--- tegelased/src/pages/Avaleht.js
+++ tegelased/src/pages/Avaleht.js
@@ -1,7 +1,41 @@
+import {useState} from "react";
 
 function Avaleht() {
+
+    
+
+
+
+    const tegelased = [
+        {eesnimi: "Mickey", perenimi: "Mouse", elukoht: "Disneyland" },
+        {eesnimi: "Minnie", perenimi: "Mouse", elukoht: "Disneyland" },
+        {eesnimi: "Winnie", perenimi: "Pooh", elukoht: "Hundred Acre Wood" },
+        {eesnimi: "Roo", perenimi: "Kangaroo", elukoht: "Hundred Acre Wood" },
+        {eesnimi: "Scooby", perenimi: "Doo", elukoht: "Cristal Cove" }
+    ]
+
+    const [klikitudNimi, uuendaKlikitudNimi] = useState("");
+
+    const kuvaNimi = (tegelane) => {
+       // console.log(tegelane.eesnimi);
+       uuendaKlikitudNimi(tegelane.eesnimi);
+    }
   return (
     <div>
+        {klikitudNimi !== "" && <div>Klikkisid tegelase {klikitudNimi} peale</div>}
+        {tegelased.map((tegelane, index) =>
+        <div key={index}>
+            <div>{tegelane.eesnimi}</div>
+            <div>{tegelane.perenimi}</div>
+            <div>{tegelane.elukoht}</div>
+            <button onClick={() => kuvaNimi(tegelane)}>Kuva nimi</button>
+        </div>
+        )
+        }
+        <br />
+        -----------------------------------------------
+        <br /><br />
+
         <div>
             <div>Mickey</div>
             <div>Mouse</div>
